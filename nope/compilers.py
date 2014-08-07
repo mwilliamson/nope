95fa3b23ba9d89156f0dd3c40338f5e0236ecf00
Michael Williamson <mike@zwobble.org>
2014-08-07 19:02:47 +0100

    Use Python 3 style division in Python 2

--- nope/compilers.py
+++ nope/compilers.py
@@ -13,8 +13,36 @@ class Python2(object):
     binary = "python2"
     extension = "py"
     
-    def compile(self, source, nope_ast, destination_dir):
-        _copy_recursive(source, destination_dir)
+    def compile(self, source_path, nope_ast, destination_dir):
+        def _source_path_to_dest_path(source_full_path):
+            if os.path.isfile(source_path):
+                root = os.path.dirname(source_path)
+            else:
+                root = source_path
+            relative_path = os.path.relpath(source_full_path, root)
+            return os.path.join(destination_dir, relative_path)
+        
+        def handle_dir(path):
+            os.mkdir(_source_path_to_dest_path(path))
+        
+        def handle_file(path):
+            with open(path) as source_file:
+                with open(_source_path_to_dest_path(path), "w") as dest_file:
+                    started = False
+                    while True:
+                        line = source_file.readline()
+                        
+                        if line == "":
+                            return
+                        
+                        if not started and not line.startswith("#"):
+                            started = True
+                            for future in ["division", "absolute_import", "print_function", "unicode_literals"]:
+                                dest_file.write("from __future__ import {}\n".format(future))
+                                
+                        dest_file.write(line)
+        
+        _walk_tree(source_path, handle_dir, handle_file)
 
 
 class Python3(object):
@@ -40,18 +68,28 @@ def _copy_recursive(source_path, dest_path):
         relative_path = os.path.relpath(source_full_path, source_path)
         return os.path.join(dest_path, relative_path)
     
-    if os.path.isdir(source_path):
-        for root, dirnames, filenames in os.walk(source_path):
+    def handle_dir(path):
+        os.mkdir(_source_path_to_dest_path(path))
+    
+    def handle_file(path):
+        shutil.copy(path, _source_path_to_dest_path(path))
+    
+    _walk_tree(source_path, handle_dir, handle_file)
+
+
+def _walk_tree(path, handle_dir, handle_file):
+    if os.path.isdir(path):
+        for root, dirnames, filenames in os.walk(path):
             for dirname in dirnames: 
                 full_path = os.path.join(root, dirname)
-                os.mkdir(_source_path_to_dest_path(full_path))
+                handle_dir(full_path)
             
             for filename in filenames:
                 full_path = os.path.join(root, filename)
-                _copy_recursive(full_path, _source_path_to_dest_path(full_path))
+                handle_file(full_path)
     else:
-        shutil.copy(source_path, dest_path)
-    
+        handle_file(path)
+
 
 
 _all = [
