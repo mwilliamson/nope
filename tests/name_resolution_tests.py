9520413165bff7e03297e461f877a044c96481b9
Michael Williamson <mike@zwobble.org>
2014-08-25 16:00:23 +0100

    Remove old TODO

--- tests/name_resolution_tests.py
+++ tests/name_resolution_tests.py
@@ -342,7 +342,6 @@ def except_handler_target_is_defined_but_not_definitely_bound():
 
 @istest
 def except_handler_targets_in_same_try_statement_can_share_their_name():
-    # TODO: this isn't true when exception handlers are nested
     # TODO: exception handler targets can't be used in nested functions
     context = _new_context()
     node = nodes.try_statement(
