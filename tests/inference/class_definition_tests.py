02a38d79b0382a3b5c8cbfd9739dec6e0cb0c30e
Michael Williamson <mike@zwobble.org>
2014-09-01 22:13:10 +0100

    Split class definition test into two

--- tests/inference/class_definition_tests.py
+++ tests/inference/class_definition_tests.py
@@ -7,18 +7,29 @@ from .util import assert_type_mismatch, update_context, SingleScopeReferences
 
 
 @istest
-def empty_class_definition_updates_type_to_meta_class_of_scalar_class():
+def class_type_uses_name_from_node():
     node = nodes.class_def("User", [])
-    meta_type = _infer_class_type(node)
-    assert isinstance(meta_type, types.MetaType)
-    class_type = meta_type.type
-    assert isinstance(class_type, types._ScalarType)
+    class_type = _infer_class_type(node)
     assert_equal("User", class_type.name)
-    assert_equal(types.func([], class_type), meta_type.attrs.type_of("__call__"))
+
+
+@istest
+def class_constructor_takes_no_args_and_returns_class_if_init_not_set():
+    node = nodes.class_def("User", [])
+    meta_type = _infer_meta_type(node)
+    assert_equal(types.func([], meta_type.type), meta_type.attrs.type_of("__call__"))
     
 
 
-def _infer_class_type(class_node):
+def _infer_meta_type(class_node):
     context = Context(SingleScopeReferences(), {}).enter_module()
     update_context(class_node, context)
-    return context.lookup(class_node)
+    meta_type = context.lookup(class_node)
+    assert isinstance(meta_type, types.MetaType)
+    assert isinstance(meta_type.type, types._ScalarType)
+    return meta_type
+
+
+def _infer_class_type(class_node):
+    return _infer_meta_type(class_node).type
+    
