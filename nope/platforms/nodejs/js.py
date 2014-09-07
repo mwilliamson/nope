62a1c2314260a044a5af75db0c722277b5e35a54
Michael Williamson <mike@zwobble.org>
2014-09-07 18:11:38 +0100

    Use dodge.data_class instead of collections.namedtuple

--- nope/platforms/nodejs/js.py
+++ nope/platforms/nodejs/js.py
@@ -1,6 +1,7 @@
-import collections
 import io
 import json
+
+import dodge
 
 
 def dump(obj, fileobj):
@@ -213,31 +214,31 @@ def _serialize_object(obj, fileobj):
         
     fileobj.write("}")
 
-statements = Statements = collections.namedtuple("Statements", ["statements"])
+statements = Statements = dodge.data_class("Statements", ["statements"])
 
-expression_statement = ExpressionStatement = collections.namedtuple("ExpressionStatement", ["value"])
-function_declaration = FunctionDeclaration = collections.namedtuple("FunctionDeclaration", ["name", "args", "body"])
-function_expression = FunctionExpression = collections.namedtuple("FunctionExpression", ["args", "body"])
-ret = ReturnStatement = collections.namedtuple("ReturnStatement", ["value"])
+expression_statement = ExpressionStatement = dodge.data_class("ExpressionStatement", ["value"])
+function_declaration = FunctionDeclaration = dodge.data_class("FunctionDeclaration", ["name", "args", "body"])
+function_expression = FunctionExpression = dodge.data_class("FunctionExpression", ["args", "body"])
+ret = ReturnStatement = dodge.data_class("ReturnStatement", ["value"])
 
-VariableDeclaration = collections.namedtuple("VariableDeclaration", ["name", "value"])
+VariableDeclaration = dodge.data_class("VariableDeclaration", ["name", "value"])
 
 def var(name, value=None):
     return VariableDeclaration(name, value)
 
-if_else = IfElse = collections.namedtuple("IfElse", ["condition", "true_body", "false_body"])
-TryCatch = collections.namedtuple("TryCatch", ["try_body", "error_name", "catch_body", "finally_body"])
+if_else = IfElse = dodge.data_class("IfElse", ["condition", "true_body", "false_body"])
+TryCatch = dodge.data_class("TryCatch", ["try_body", "error_name", "catch_body", "finally_body"])
 
 def try_catch(try_body, error_name=None, catch_body=None, finally_body=None):
     return TryCatch(try_body, error_name, catch_body, finally_body)
     
 
-while_loop = WhileLoop = collections.namedtuple("WhileLoop", ["condition", "body"])
-break_statement = BreakStatement = collections.namedtuple("BreakStatement", [])
-continue_statement = ContinueStatement = collections.namedtuple("ContinueStatement", [])
-throw = Throw = collections.namedtuple("Throw", ["value"])
+while_loop = WhileLoop = dodge.data_class("WhileLoop", ["condition", "body"])
+break_statement = BreakStatement = dodge.data_class("BreakStatement", [])
+continue_statement = ContinueStatement = dodge.data_class("ContinueStatement", [])
+throw = Throw = dodge.data_class("Throw", ["value"])
 
-Assignment = collections.namedtuple("Assignment", ["target", "value"])
+Assignment = dodge.data_class("Assignment", ["target", "value"])
 
 def assign(target, value):
     if isinstance(target, str):
@@ -248,18 +249,18 @@ def assign(target, value):
 def assign_statement(target, value):
     return expression_statement(assign(target, value))
 
-property_access = PropertyAccess = collections.namedtuple("PropertyAccess", ["value", "property"])
-binary_operation = BinaryOperation = collections.namedtuple("BinaryOperation", ["operator", "left", "right"])
-unary_operation = UnaryOperation = collections.namedtuple("UnaryOperation", ["operator", "operand"])
-call = Call = collections.namedtuple("Call", ["func", "args"])
-ref = VariableReference = collections.namedtuple("VariableReference", ["name"])
-number = Number = collections.namedtuple("Number", ["value"])
-NullLiteral = collections.namedtuple("NullLiteral", [])
+property_access = PropertyAccess = dodge.data_class("PropertyAccess", ["value", "property"])
+binary_operation = BinaryOperation = dodge.data_class("BinaryOperation", ["operator", "left", "right"])
+unary_operation = UnaryOperation = dodge.data_class("UnaryOperation", ["operator", "operand"])
+call = Call = dodge.data_class("Call", ["func", "args"])
+ref = VariableReference = dodge.data_class("VariableReference", ["name"])
+number = Number = dodge.data_class("Number", ["value"])
+NullLiteral = dodge.data_class("NullLiteral", [])
 null = NullLiteral()
-boolean = Boolean = collections.namedtuple("Boolean", ["value"])
-string = String = collections.namedtuple("String", ["value"])
-array = Array = collections.namedtuple("Array", ["elements"])
-obj = Object = collections.namedtuple("Object", ["properties"])
+boolean = Boolean = dodge.data_class("Boolean", ["value"])
+string = String = dodge.data_class("String", ["value"])
+array = Array = dodge.data_class("Array", ["elements"])
+obj = Object = dodge.data_class("Object", ["properties"])
 
 _serializers = {
     Statements: _serialize_statements,
