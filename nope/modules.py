1574cc98419c8036348448ccc97983035bbde17b
Michael Williamson <mike@zwobble.org>
2014-10-12 20:50:30 +0100

    Remove statefulness in ModuleExports

--- nope/modules.py
+++ nope/modules.py
@@ -19,35 +19,44 @@ class ModuleExports(zuice.Base):
     _declaration_finder = zuice.dependency(name_declaration.DeclarationFinder)
     
     def names(self, module_node):
-        export_names = None
+        export_declarations = self._export_declarations(module_node.body)
         
-        for statement in module_node.body:
-            if (isinstance(statement, nodes.Assignment) and
-                    any(isinstance(target, nodes.VariableReference) and target.name == "__all__" for target in statement.targets)):
-                if not isinstance(statement.value, nodes.ListLiteral):
-                    raise _all_wrong_type_error(statement)
-                
-                def extract_string_value(node):
-                    if isinstance(node, nodes.StringExpression):
-                        return node.value
-                    else:
-                        raise _all_wrong_type_error(statement)
-                
-                if export_names is None:
-                    export_names = [
-                        extract_string_value(element)
-                        for element in statement.value.elements
-                    ]
-                else:
-                    raise errors.AllAssignmentError(statement, "__all__ cannot be redeclared")
-        
-        if export_names is None:
+        if len(export_declarations) == 0:
             return [
                 name for name in self._declaration_finder.declarations_in_module(module_node).names()
                 if not name.startswith("_")
             ]
+        elif len(export_declarations) == 1:
+            statement, = export_declarations
+            
+            if not isinstance(statement.value, nodes.ListLiteral):
+                raise _all_wrong_type_error(statement)
+            
+            return [
+                _extract_string_value_from_literal(statement, element)
+                for element in statement.value.elements
+            ]
         else:
-            return export_names
+            raise errors.AllAssignmentError(export_declarations[1], "__all__ cannot be redeclared")
+    
+    def _export_declarations(self, statements):
+        return list(filter(self._is_export_declaration, statements))
+    
+    def _is_export_declaration(self, statement):
+        return (
+            isinstance(statement, nodes.Assignment) and
+            any(
+                isinstance(target, nodes.VariableReference) and
+                target.name == "__all__" for target in statement.targets
+            )
+        )
+
+
+def _extract_string_value_from_literal(statement, node):
+    if isinstance(node, nodes.StringExpression):
+        return node.value
+    else:
+        raise _all_wrong_type_error(statement)
 
 
 def _all_wrong_type_error(node):
