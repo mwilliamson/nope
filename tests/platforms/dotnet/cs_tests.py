ec6b57c4ec7bcf3e93140a88cf5582439275fcdd
Michael Williamson <mike@zwobble.org>
2015-02-22 11:06:58 +0000

    Support proper serialization for try/catch/finally in C#

---
+++ tests/platforms/dotnet/cs_tests.py
@@ -0,0 +1,65 @@
+import collections
+
+from nose.tools import istest, assert_equal
+
+from nope.platforms.dotnet import cs
+
+
+@istest
+def test_serialize_try_with_finally_body():
+    node = cs.try_(
+        [cs.ret(cs.ref("x"))],
+        finally_body=[cs.expression_statement(cs.ref("y"))],
+    )
+    expected = """try {
+    return x;
+} finally {
+    y;
+}
+"""
+    assert_equal(expected, cs.dumps(node))
+
+
+@istest
+def test_serialize_try_with_unfiltered_catch():
+    node = cs.try_(
+        [cs.ret(cs.ref("x"))],
+        handlers=[cs.catch(None, None, [cs.expression_statement(cs.ref("y"))])],
+    )
+    expected = """try {
+    return x;
+} catch {
+    y;
+}
+"""
+    assert_equal(expected, cs.dumps(node))
+
+
+@istest
+def test_serialize_try_with_filtered_catch_without_name():
+    node = cs.try_(
+        [cs.ret(cs.ref("x"))],
+        handlers=[cs.catch(cs.ref("Exception"), None, [cs.expression_statement(cs.ref("y"))])],
+    )
+    expected = """try {
+    return x;
+} catch (Exception) {
+    y;
+}
+"""
+    assert_equal(expected, cs.dumps(node))
+
+
+@istest
+def test_serialize_try_with_filtered_catch_with_name():
+    node = cs.try_(
+        [cs.ret(cs.ref("x"))],
+        handlers=[cs.catch(cs.ref("Exception"), "exception", [cs.expression_statement(cs.ref("y"))])],
+    )
+    expected = """try {
+    return x;
+} catch (Exception exception) {
+    y;
+}
+"""
+    assert_equal(expected, cs.dumps(node))
